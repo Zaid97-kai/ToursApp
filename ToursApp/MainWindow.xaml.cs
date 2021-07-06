2479999cdd7bb5e73c6cdcfb514a71faec6c15b9
zzmingaliev <zzmingaliev@gmail.com>
2021-07-06 12:06:47 +0300

    Добавлена фильтрация

--- ToursApp/MainWindow.xaml.cs
+++ ToursApp/MainWindow.xaml.cs
@@ -21,10 +21,40 @@ namespace ToursApp
     public partial class MainWindow : Window
     {
         private ToursDB_08Entities _context = new ToursDB_08Entities();
+        private List<Tour> _tours = new List<Tour>();
+        private string _SelectedType = "";
+        private string _FindedName = "";
         public MainWindow()
         {
             InitializeComponent();
             ListTours.ItemsSource = _context.Tours.ToList();
+            CmbTypes.ItemsSource = _context.Types.OrderBy(types => types.Name).ToList();
+
+            this._tours = _context.Tours.ToList();
+        }
+
+        public void RefreshTours()
+        {
+            _tours = (from t in _tours
+                     from tn in t.Types
+                     where tn.Name == _SelectedType
+                     select t).ToList();
+
+            ListTours.ItemsSource = _tours;
+        }
+
+        private void TxtFindedTourName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this._FindedName = TxtFindedTourName.Text;
+        }
+
+        private void CmbTypes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this._tours = _context.Tours.ToList();
+
+            Type type = CmbTypes.SelectedItem as Type;
+            this._SelectedType = type.Name;
+            RefreshTours();
         }
     }
 }
